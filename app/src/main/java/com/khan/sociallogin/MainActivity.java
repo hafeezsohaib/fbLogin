5a447b80f014481ff467194249cd2cadd5f8a282
s.khanhuulke.com <hrhk@1234>
2019-02-14 15:04:03 +0530

    Fisrst Commit

--- app/src/main/java/com/khan/sociallogin/MainActivity.java
+++ app/src/main/java/com/khan/sociallogin/MainActivity.java
@@ -3,11 +3,14 @@ package com.khan.sociallogin;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.khan.mysociallogin.MyLogin;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
---
+++ mysociallogin/src/main/java/com/khan/mysociallogin/MyLogin.java
@@ -0,0 +1,8 @@
+package com.khan.mysociallogin;
+
+public class MyLogin {
+
+    public static  int Login(int a,int b){
+        return a+b;
+    }
+}
